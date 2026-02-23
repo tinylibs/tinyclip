9fa33c5f0884e87e238ed11e49dfc268e38a1257
James Garbutt <43081j@users.noreply.github.com>
2026-02-23 16:37:28 +0000

    initial boilerplate

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+export async function readText(): Promise<string> {
+  return ''; // TODO
+}
+
+export async function writeText(_text: string): Promise<void> {
+  return; // TODO
+}
+
+export function readTextSync(): string {
+  return ''; // TODO
+}
+
+export function writeTextSync(_text: string): void {
+  return; // TODO
+}
