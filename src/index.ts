8a6ce587c284031c13fbbd6fb8a4e25469cf12a0
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-25 14:40:54 +0100

    feat: more robust impl

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,46 @@
 /// <reference types="node" />
-import {
-  spawn,
-  SpawnOptionsWithoutStdio,
-  SpawnOptionsWithStdioTuple,
-  StdioNull,
-  StdioPipe
-} from 'node:child_process';
+import {spawn} from 'node:child_process';
 
 const TIMEOUT = 2000;
 
-function checkUnixCommandExists(command: string): Promise<boolean> {
-  return new Promise((resolve) => {
-    const proc = spawn('which', [command]);
-    proc.on('error', () => resolve(false));
-    proc.on('close', (code) => resolve(code === 0));
-  });
-}
-
 type Command = [string, Array<string>];
 
-const WINDOWS_READ_COMMAND: Command = ['powershell', ['Get-Clipboard']];
-
-const UNIX_READ_COMMANDS: Array<Command> = [
-  ['wl-paste', []],
-  ['xsel', ['--clipboard', '--output']],
-  ['xclip', ['-selection', 'clipboard', '-o']],
-  // wsl
-  WINDOWS_READ_COMMAND
-];
+function getReadCommand(): Command | undefined {
+  switch (process.platform) {
+    case 'darwin':
+      return ['pbpaste', []];
+    case 'win32':
+      return ['powershell', ['Get-Clipboard']];
+    case 'linux':
+    case 'freebsd':
+    case 'openbsd':
+      if (process.env.WAYLAND_DISPLAY) {
+        return ['wl-paste', []];
+      }
+      if (process.env.WSL_DISTRO_NAME) {
+        return ['powershell.exe', ['-noprofile', '-command', 'Get-Clipboard']];
+      }
+      return ['xclip', ['-selection', 'clipboard', '-o']];
+    case 'android':
+      return ['get', []];
+    default:
+      return undefined;
+  }
+}
 
 /**
  * Reads text from the clipboard.
  */
 export function readText(): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    let proc;
-    const options: SpawnOptionsWithoutStdio = {
-      signal: AbortSignal.timeout(TIMEOUT)
-    };
-
-    if (process.platform === 'darwin') {
-      proc = spawn('pbpaste', options);
-    } else if (process.platform === 'win32') {
-      proc = spawn(...WINDOWS_READ_COMMAND, options);
-    } else {
-      // Unix: check if a supported command is installed
-      for (const [unixCommand, unixArgs] of UNIX_READ_COMMANDS) {
-        const exists = await checkUnixCommandExists(unixCommand);
-        if (exists) {
-          proc = spawn(unixCommand, unixArgs, options);
-          break;
-        }
-      }
+    const command = getReadCommand();
+    if (!command) {
+      return reject(new Error('No clipboard tool found'));
     }
 
-    if (!proc) return reject(new Error('No clipboard tool found'));
+    const proc = spawn(...command, {
+      signal: AbortSignal.timeout(TIMEOUT)
+    });
 
     let data = '';
     proc.stdout.on('data', (chunk) => (data += chunk));
@@ -73,44 +59,43 @@ export function readText(): Promise<string> {
   });
 }
 
-const WINDOWS_WRITE_COMMAND: Command = ['clip', []];
-
-const UNIX_WRITE_COMMANDS: Array<Command> = [
-  ['wl-copy', []],
-  ['xsel', ['--clipboard', '--input']],
-  ['xclip', ['-selection', 'clipboard', '-i']],
-  // wsl
-  WINDOWS_WRITE_COMMAND
-];
+function getWriteCommand(): Command | undefined {
+  switch (process.platform) {
+    case 'darwin':
+      return ['pbcopy', []];
+    case 'win32':
+      return ['clip', []];
+    case 'linux':
+    case 'freebsd':
+    case 'openbsd':
+      if (process.env.WSL_DISTRO_NAME) {
+        return ['clip.exe', []];
+      }
+      if (process.env.WAYLAND_DISPLAY) {
+        return ['wl-copy', []];
+      }
+      return ['xclip', ['-selection', 'clipboard', '-i']];
+    case 'android':
+      return ['termux-clipboard-set', []];
+    default:
+      return undefined;
+  }
+}
 
 /**
  * Writes text to the clipboard.
  */
 export function writeText(text: string): Promise<void> {
   return new Promise(async (resolve, reject) => {
-    let proc;
-    const options: SpawnOptionsWithStdioTuple<StdioPipe, StdioNull, StdioNull> =
-      {
-        stdio: ['pipe', 'ignore', 'ignore'],
-        signal: AbortSignal.timeout(TIMEOUT)
-      };
-
-    if (process.platform === 'darwin') {
-      proc = spawn('pbcopy', options);
-    } else if (process.platform === 'win32') {
-      proc = spawn(...WINDOWS_WRITE_COMMAND, options);
-    } else {
-      // Unix: check if a supported command is installed
-      for (const [unixCommand, unixArgs] of UNIX_WRITE_COMMANDS) {
-        const exists = await checkUnixCommandExists(unixCommand);
-        if (exists) {
-          proc = spawn(unixCommand, unixArgs, options);
-          break;
-        }
-      }
+    const command = getWriteCommand();
+    if (!command) {
+      return reject(new Error('No clipboard tool found'));
     }
 
-    if (!proc) return reject(new Error('No clipboard tool found'));
+    const proc = spawn(...command, {
+      stdio: ['pipe', 'ignore', 'ignore'],
+      signal: AbortSignal.timeout(TIMEOUT)
+    });
 
     proc.on('error', (cause) =>
       reject(new Error('An error occurred while copying', {cause}))
