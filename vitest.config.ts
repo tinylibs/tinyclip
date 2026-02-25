97774ee92404bd44a1487c60395702aa6af9390b
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-25 19:25:43 +0100

    feat: browser tests (#9)
    * feat: browser tests
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * fix: ci
    
    * Update .github/workflows/main.yml
    
    Co-authored-by: James Garbutt <43081j@users.noreply.github.com>
    
    * Apply suggestion from @43081j
    
    ---------
    
    Co-authored-by: James Garbutt <43081j@users.noreply.github.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,38 @@
 import {defineConfig} from 'vitest/config';
+import {playwright} from '@vitest/browser-playwright';
 
 export default defineConfig({
   test: {
     coverage: {
       provider: 'v8'
     },
-    include: ['tests/**/*.test.ts']
+    projects: [
+      {
+        extends: './vitest.config.ts',
+        test: {
+          include: ['tests/index.test.ts'],
+          environment: 'node',
+          name: 'node'
+        }
+      },
+      {
+        extends: './vitest.config.ts',
+        test: {
+          include: ['tests/browser.test.ts'],
+          name: 'browser',
+          browser: {
+            enabled: true,
+            provider: playwright({
+              contextOptions: {
+                permissions: ['clipboard-read', 'clipboard-write']
+              }
+            }),
+            headless: true,
+            instances: [{browser: 'chromium'}],
+            screenshotFailures: false
+          }
+        }
+      }
+    ]
   }
 });
