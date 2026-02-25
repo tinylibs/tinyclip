160bab0040ba0ee49c2ea59dd8f0e2833b1981b7
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-02-25 08:19:18 +0100

    feedback

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,5 @@
 import {describe, it, expect} from 'vitest';
-import * as clipboard from './index.js';
+import * as clipboard from '../src/index.js';
 
 describe('clipboard', () => {
   it('should export correct API', () => {
