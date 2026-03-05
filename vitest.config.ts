6f41e3484867b4da678d3cea56b69e0a1280bcd7
Florian Lefebvre <contact@florian-lefebvre.dev>
2026-03-05 10:49:34 +0100

    feedback

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,6 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['tests/*.test.ts'],
-    environment: 'node',
-    name: 'node',
     coverage: {
       provider: 'v8'
     }
